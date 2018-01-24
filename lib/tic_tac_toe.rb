07093cd43986768059be88f885aefa5bb53b3680
chandana M <chandu11288@gmail.com>
2018-01-24 21:17:27 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -90,8 +90,8 @@ class TicTacToe
     end
     no_combinations = true
     WIN_COMBINATIONS.each do |combination|
-      if combination1 = board[combination[0]] == "X" && board[combination[1]] == "X" && board[combination[2]] == "X" ||
-         combination2 = board[combination[0]] == "O" && board[combination[1]] == "O" && board[combination[2]] == "O"
+      if combination1 = @board[combination[0]] == "X" && @board[combination[1]] == "X" && @board[combination[2]] == "X" ||
+         combination2 = @board[combination[0]] == "O" && @board[combination[1]] == "O" && @board[combination[2]] == "O"
           no_combinations = false
           return combination
       end
@@ -101,9 +101,9 @@ class TicTacToe
     end
   end
 
-  #full method should accept a board and return true if every element in the board contains either an "X" or an "O".
-  def full?(board)
-    any_spaces =  board.any?{|element| element == " "}
+  #full method return true if every element in the board contains either an "X" or an "O".
+  def full?
+    any_spaces =  @board.any?{|element| element == " "}
       if any_spaces == true
         return false
       else
@@ -111,10 +111,10 @@ class TicTacToe
       end
   end
 
-  #accepts a board and returns true if the board has not been won and is full and false if the board is not won and the board is not full, and false if the board is won
-  def draw?(board)
-    incomplete_board = !full?(board)
-    is_game_won = won?(board)
+  #draw? method returns true if the board has not been won and is full and false if the board is not won and the board is not full, and false if the board is won
+  def draw?
+    incomplete_board = !full?
+    is_game_won = won?
       if incomplete_board == true
         return false
       elsif is_game_won.is_a?(Array)
